41a9389bf5f0c1e98248406ca56523db005d9e96
Calum Grant <calum74@gmail.com>
2026-01-04 10:28:35 +0000

    Split up object files for Windows

--- src/dynamic/containers.cpp
+++ src/dynamic/containers.cpp
@@ -1,56 +1,13 @@
 #include <cutty/dynamic/instantiate.hpp>
 
-#include <deque>
-#include <iostream>
-#include <map>
-#include <set>
 #include <unordered_map>
 #include <unordered_set>
-#include <vector>
 
 namespace cy = cutty;
 
-using list_type = std::vector<cy::dynamic>;
-using map_type = std::map<cy::dynamic, cy::dynamic>;
-using set_type = std::set<cy::dynamic>;
 using unordered_map_type = std::unordered_map<cy::dynamic, cy::dynamic>;
 using unordered_set_type = std::unordered_set<cy::dynamic>;
-using queue_type = std::deque<cy::dynamic>;
 using object_type = std::unordered_map<std::string, cy::dynamic>;
-
-std::ostream &operator<<(std::ostream &os, const list_type &);
-std::ostream &operator<<(std::ostream &os, const map_type &);
-std::ostream &operator<<(std::ostream &os, const unordered_map_type &);
-std::ostream &operator<<(std::ostream &os, const set_type &);
-std::ostream &operator<<(std::ostream &os, const unordered_set_type &);
-std::ostream &operator<<(std::ostream &os, const queue_type &);
-
-template<typename T> requires (
-    std::same_as<std::map<cy::dynamic, cy::dynamic>, std::remove_cvref_t<T>>
-    ||     std::same_as<std::set<cy::dynamic>, std::remove_cvref_t<T>>
-    )
-struct cy::dynamic::traits<T> : public default_traits<T>
-{
-   // rbegin and rend are broken 
-   // because rbegin(c) + n does not compile.
-   static dynamic rbegin(auto&&)
-   {
-        throw unsupported("rbegin()");
-   }  
-
-   static dynamic rend(auto&&)
-   {
-        throw unsupported("rend()");
-   }  
-};
-
-
-CY_INSTANTIATE(std::vector<cy::dynamic>)
-
-cy::dynamic cy::dynamic::list()
-{
-    return dynamic(std::vector<dynamic>{});
-}
 
 CY_INSTANTIATE(object_type);
 
@@ -59,50 +16,9 @@ cy::dynamic cy::dynamic::object()
     return dynamic(object_type{});
 }
 
-cy::dynamic cy::dynamic::list(std::initializer_list<dynamic> items)
-{
-    auto l = dynamic(std::vector<dynamic>{});
-    for (auto &item : items)
-        l.push_back(item);
-    return l;
-}
-
-CY_INSTANTIATE(map_type)
-
-cy::dynamic cy::dynamic::map()
-{
-    return dynamic(map_type{});
-}
-
 cy::dynamic cy::dynamic::dict()
 {
     return dynamic(unordered_map_type{});
-}
-
-cy::dynamic cy::dynamic::set()
-{
-    return dynamic(set_type{});
-}
-
-cy::dynamic cy::dynamic::set(std::initializer_list<dynamic> items)
-{
-    auto s = dynamic::set();
-    for (auto &item : items)
-        s.insert(item);
-    return s;
-}
-
-cy::dynamic cy::dynamic::queue()
-{
-    return dynamic(queue_type{});
-}
-
-cy::dynamic cy::dynamic::queue(std::initializer_list<dynamic> items)
-{
-    auto s = dynamic::queue();
-    for (auto &item : items)
-        s.push_back(item);
-    return s;
 }
 
 cy::dynamic cy::dynamic::dict(std::initializer_list<std::pair<dynamic, dynamic>> items)
@@ -111,12 +27,4 @@ cy::dynamic cy::dynamic::dict(std::initializer_list<std::pair<dynamic, dynamic>>
     for (auto &&[x, y] : items)
         d.insert(dynamic(std::move(x)), dynamic(std::move(y)));
     return d;
-}
-
-cy::dynamic cy::dynamic::map(std::initializer_list<std::pair<dynamic, dynamic>> items)
-{
-    auto m = dynamic::map();
-    for (auto &&[x, y] : items)
-        m.insert(dynamic(std::move(x)), dynamic(std::move(y)));
-    return m;
 }
--- src/dynamic/types.cpp
+++ src/dynamic/types.cpp
@@ -21,23 +21,6 @@ std::ostream &cy::operator<<(std::ostream &os, cy::dynamic::empty)
 }
 
 CY_INSTANTIATE(cy::dynamic::empty)
-CY_INSTANTIATE(bool)
-CY_INSTANTIATE(signed char)
-CY_INSTANTIATE(unsigned char)
-CY_INSTANTIATE(char)
-
-CY_INSTANTIATE(short)
-CY_INSTANTIATE(unsigned short)
-CY_INSTANTIATE(int)
-CY_INSTANTIATE(unsigned int)
-CY_INSTANTIATE(long)
-CY_INSTANTIATE(unsigned long)
-CY_INSTANTIATE(long long)
-CY_INSTANTIATE(unsigned long long)
-
-CY_INSTANTIATE(float)
-CY_INSTANTIATE(double)
-
 CY_INSTANTIATE(std::string)
 CY_INSTANTIATE(char *)
 CY_INSTANTIATE(std::string_view)
